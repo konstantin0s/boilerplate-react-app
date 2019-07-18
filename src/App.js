f60c0600e14c04af0769ffd94e45ea4361e4d892
konstantin0s <constantintofan85@gmail.com>
2019-07-18 18:32:42 +0200

    Add boostrapt and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Autocomplet app</h2>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>Welcome to your crazy app.</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
